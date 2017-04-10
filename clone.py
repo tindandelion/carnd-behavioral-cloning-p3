5cd2221df6d302e28a5f28efd995a6cfc24dd29c
Sergey Moshnikov <s.moshnikov@gmail.com>
2017-04-10 03:54:39 +0000

    Training model

--- clone.py
+++ clone.py
@@ -2,22 +2,22 @@ import csv
 import cv2
 import numpy as np
 
-def read_csv():
-    with open('data/driving_log.csv', 'r') as f:
+def read_csv(root_path):
+    with open(root_path + 'driving_log.csv', 'r') as f:
         reader = csv.reader(f)
         for line in reader:
             yield line
 
-def make_local_path(path):
+def make_local_path(path, local_root):
     basename = path.split('/')[-1]
-    return './data/IMG/' + basename
+    return local_root + 'IMG/' + basename
 
 def load_image(local_path):
     return cv2.imread(local_path)
 
-def extract_images(csv_line):
+def extract_images(csv_line, local_root):
     paths = csv_line[0:3]
-    return [load_image(make_local_path(p)) for p in paths]
+    return [load_image(make_local_path(p, local_root)) for p in paths]
 
 def extract_measurements(csv_line):
     return float(csv_line[3])
@@ -25,12 +25,23 @@ def extract_measurements(csv_line):
 images = []
 measurements = []
 
-for line in read_csv():
-    images.append(extract_images(line)[0])
+forward_img = './data/forward/'
+reverse_img = './data/reverse/'
+
+print("Reading forward images...\n")
+for line in read_csv(forward_img):
+    images.append(extract_images(line, forward_img)[0])
+    measurements.append(extract_measurements(line))
+
+print("Reading reverse images...\n")
+for line in read_csv(reverse_img):
+    images.append(extract_images(line, reverse_img)[0])
     measurements.append(extract_measurements(line))
 
 print(len(images), len(measurements))
     
+print("Training model...")
+
 X_train = np.array(images)
 y_train = np.array(measurements)
 
